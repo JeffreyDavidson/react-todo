48e9835de811796025fb0d7c4a4c634a7c6efaee
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-05-13 15:01:37 -0400

    Other built-in React Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import '../reset.css';
 import '../App.css';
 import NoTodos from './NoTodos';
-import { useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
+  const [name, setName] = useState('');
+  const nameInputEl = useRef(null);
   const [todos, setTodos] = useState([
       {
           id: 1,
@@ -100,9 +102,11 @@ function App() {
     setTodos(updatedTodos);
   }
 
- function remaining() {
+ function remainingCalculation() {
     return todos.filter(todo => !todo.isComplete).length;
  }
+
+ const remaining = useMemo(remainingCalculation, [todos]);
 
  function clearCompleted() {
     setTodos([...todos].filter(todo => !todo.isComplete));
@@ -128,9 +132,30 @@ function App() {
       }
  }
 
+ useEffect(() => {
+    nameInputEl.current.focus();
+
+    return function cleanup() {
+
+    };
+ }, [])
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
+        <div className="name-container">
+          <h2>What is your name?</h2>
+          <form action="#">
+            <input
+              type="text"
+              ref={nameInputEl}
+              className="todo-input"
+              placeholder="What is your name"
+              value={name}
+              onChange={event => setName(event.target.value)} />
+          </form>
+          {name && <p className="name-label">Hello, {name}</p>}
+        </div>
         <h2>Todo App</h2>
         <TodoForm addTodo={addTodo} />
 
